88393399cac45f70cc827d3b452aa95c0a224d51
Sander van Zoest <sander@switchcasegroup.com>
2019-06-18 20:28:42 -0700

    fix references

---
+++ deck.js
@@ -0,0 +1,27 @@
+export { dark as theme } from 'mdx-deck/themes';
+
+import { slides as begin } from './begin.mdx'
+import { slides as ide } from './ide.mdx'
+import { slides as transpiling } from './transpiling.mdx'
+import { slides as linting } from './linting.mdx'
+import { slides as pkginit } from './package/init.mdx'
+import { slides as unittesting } from './testing/unit.mdx'
+import { slides as building } from './building.mdx'
+import { slides as intregrationtesting } from './testing/integration.mdx'
+import { slides as publishing } from './package/publishing.mdx'
+import { slides as security } from './security.mdx'
+import { slides as end } from './end.mdx'
+
+export const slides = [
+   ...begin,
+   ...ide,
+   ...transpiling,
+   ...linting,
+   ...pkginit,
+   ...unittesting,
+   ...building,
+   ...integrationtesting,
+   ...publishing,
+   ...security,
+   ...end
+]
