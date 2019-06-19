e9f8a3d4b187cac7f1d6a1124485f917fb656767
zachawilson <zach@switchcasegroup.com>
2019-06-18 21:27:16 -0700

    add prettier

--- slides/deck.js
+++ slides/deck.js
@@ -1,27 +1,27 @@
-export { dark as theme } from 'mdx-deck/themes';
+export { dark as theme } from "mdx-deck/themes";
 
-import { slides as begin } from './begin.mdx'
-import { slides as ide } from './ide.mdx'
-import { slides as transpiling } from './transpiling.mdx'
-import { slides as linting } from './linting.mdx'
-import { slides as pkginit } from './package/init.mdx'
-import { slides as unittesting } from './testing/unit.mdx'
-import { slides as building } from './building.mdx'
-import { slides as integrationtesting } from './testing/integration.mdx'
-import { slides as publishing } from './package/publishing.mdx'
-import { slides as security } from './security.mdx'
-import { slides as end } from './end.mdx'
+import { slides as begin } from "./begin.mdx";
+import { slides as ide } from "./ide.mdx";
+import { slides as transpiling } from "./transpiling.mdx";
+import { slides as linting } from "./linting.mdx";
+import { slides as pkginit } from "./package/init.mdx";
+import { slides as unittesting } from "./testing/unit.mdx";
+import { slides as building } from "./building.mdx";
+import { slides as integrationtesting } from "./testing/integration.mdx";
+import { slides as publishing } from "./package/publishing.mdx";
+import { slides as security } from "./security.mdx";
+import { slides as end } from "./end.mdx";
 
 export const slides = [
-   ...begin,
-   ...ide,
-   ...transpiling,
-   ...linting,
-   ...pkginit,
-   ...unittesting,
-   ...building,
-   ...integrationtesting,
-   ...publishing,
-   ...security,
-   ...end
-]
+  ...begin,
+  ...ide,
+  ...transpiling,
+  ...linting,
+  ...pkginit,
+  ...unittesting,
+  ...building,
+  ...integrationtesting,
+  ...publishing,
+  ...security,
+  ...end
+];
