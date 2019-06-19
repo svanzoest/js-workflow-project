1ca984f789cb00de6a00497b6cc1f486ddd1215e
zachawilson <zach@switchcasegroup.com>
2019-06-18 22:15:34 -0700

    some slide notes and formatted

--- slides/deck.js
+++ slides/deck.js
@@ -1,11 +1,7 @@
-
 import dark from "@mdx-deck/themes/dark";
-import highlight from "@mdx-deck/themes/syntax-highlighter"
+import highlight from "@mdx-deck/themes/syntax-highlighter";
 
-export const themes = [
-  dark,
-  highlight
-]
+export const themes = [dark, highlight];
 
 import { slides as begin } from "./begin.mdx";
 import { slides as ide } from "./ide.mdx";
